529eb2d763b7c642086260b8d92eaddbde91e6ea
“santoshvarma18” <santosh.mutyala@zemosolabs.com>
2022-03-10 15:05:30 +0530

    Updating customer in the database

--- src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -40,8 +40,8 @@ public class CustomerDAOImpl implements CustomerDAO{
         // get current hibernate session
         Session currentSession = sessionFactory.getCurrentSession();
 
-        // save the customer
-        currentSession.save(theCustomer);
+        // save/update the customer
+        currentSession.saveOrUpdate(theCustomer);
 
     }
 
