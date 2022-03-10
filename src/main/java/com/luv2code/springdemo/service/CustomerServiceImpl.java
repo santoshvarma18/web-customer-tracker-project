1293cca83e20013ec101b2970874b68184c95c03
“santoshvarma18” <santosh.mutyala@zemosolabs.com>
2022-03-10 05:38:52 +0530

    Saving customer to database

--- src/main/java/com/luv2code/springdemo/service/CustomerServiceImpl.java
+++ src/main/java/com/luv2code/springdemo/service/CustomerServiceImpl.java
@@ -4,6 +4,7 @@ import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +18,12 @@ public class CustomerServiceImpl implements CustomerService{
     @Override
     public List<Customer> getCustomers() {
         return customerDAO.getCustomers();
+    }
+
+    @Override
+    @Transactional
+    public void saveCustomer(Customer theCustomer) {
+        customerDAO.saveCustomer(theCustomer);
     }
 
 }
